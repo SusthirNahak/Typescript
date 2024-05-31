4d316a70dc7162649fa0c56299ccf3b79dd769b7
susthir <susthir19march@gmail.com>
2024-05-31 16:45:39 +0530

    typescript datatypes

---
+++ datatypes.js
@@ -0,0 +1,6 @@
+var count = 90; //number
+var name_of_stu = "ram"; //string
+var is_avail = false; //boolean
+console.log(count);
+console.log(name_of_stu);
+console.log(is_avail);
---
+++ datatypes.ts
@@ -0,0 +1,7 @@
+let count:number=90; //number
+let name_of_stu:string="ram"; //string
+let is_avail:boolean=false; //boolean
+
+console.log(count);
+console.log(name_of_stu);
+console.log(is_avail);
